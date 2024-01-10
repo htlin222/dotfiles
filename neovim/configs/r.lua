ad445e888c1d052e0d53ab2a3d2698d85e102ac0
HsiehTing Lin <hsieh.ting.lin@gmail.com>
2024-01-11 02:23:51 +0800

    feat(neovim): 新增 nvimR 快捷鍵

--- neovim/configs/r.lua
+++ neovim/configs/r.lua
@@ -1,14 +1,18 @@
 -- https://github.com/jamespeapen/Nvim-R/wiki/Use
 return {
 	"jalvesaq/Nvim-R",
-	lazy = false,
+	lazy = false, -- must set this line
 	config = function()
 		local vim = vim
+		-- vim.g.R_assign_map = "--" -- LuaSnip might be better
 		vim.g.R_auto_start = 2
-		-- vim.g.R_assign_map = "--"
+		vim.R_objbr_auto_start = 1
 		vim.g.R_assign = 0
 		vim.g.R_show_args = 1
-		vim.keymap.set("n", "<CR>", "<Plug>RDSendLine")
+		vim.g.R_rconsole_width = 1000
+		vim.g.R_min_editor_width = 80
+		vim.g.R_args = { "--no-save", "--quiet" }
+		vim.g.R_csv_app = "tmux split-window -h vd"
 		require("core.utils").load_mappings("nvimR")
 	end,
 }
--- neovim/mappings.lua
+++ neovim/mappings.lua
@@ -165,13 +165,18 @@ M.lspsaga = {
 M.nvimR = {
 	plugin = true,
 	n = {
-		["<leader><leader>"] = { "<Plug>RDSendLine", "RDSendLine", opts = { nowait = true } }, -- 把; 指定為:
-		["<leader>rh"] = { "<Plug>Rhelp", "R help", opts = { nowait = true } }, -- 把; 指定為:
-		["<leader>rf"] = { "<cmd>Rformat<CR>", "R format", opts = { nowait = true } }, -- 把; 指定為:
-		["<leader>rq"] = { "<Plug>RSaveClose", "R save and close", opts = { nowait = true } }, -- 把; 指定為:
+		["<leader><leader>"] = { "<Plug>RDSendLine", "RDSendLine", opts = { nowait = true } },
+		["<leader><CR>"] = { "<Plug>RSendFile", "RSendFile", opts = { nowait = true } },
+		["<leader>rh"] = { "<Plug>Rhelp", "R help", opts = { nowait = true } },
+		["<leader>rf"] = { "<cmd>Rformat<CR>", "R format", opts = { nowait = true } },
+		["<leader>rl"] = { "<Plug>Rstart", "R format", opts = { nowait = true } },
+		["<leader>rq"] = { "<Plug>RSaveClose", "R save and close", opts = { nowait = true } },
+		["<leader>rd"] = { "<Plug>RViewDFv", "R save and close", opts = { nowait = true } },
+		["<leader>rs"] = { "<Plug>RSummary", "R Summary", opts = { nowait = true } },
+		["<leader>ro"] = { "<Plug>RUpdateObjBrowser", "R update object windows", opts = { nowait = true } },
 	},
 	v = {
-		["<leader><leader>"] = { "<Plug>RDSendSelection", "RDSendSelection", opts = { nowait = true } }, -- 把; 指定為:
+		["<leader><leader>"] = { "<Plug>RDSendSelection", "RDSendSelection", opts = { nowait = true } },
 	},
 }
 M.mkdn = {
