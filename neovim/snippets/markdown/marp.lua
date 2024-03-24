2913ad656124ab952cdba82999e1666852879c6e
HsiehTing Lin <hsieh.ting.lin@gmail.com>
2024-03-25 02:43:03 +0800

    feat: 新增Marp Markdown片段設定

---
+++ neovim/snippets/markdown/marp.lua
@@ -0,0 +1,16 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+return {
+	-- s({ trig = "free" }, { t("test") }),
+	s(
+		{ trig = "free" },
+		{ t({ ":::free", "", "" }), i(1), t({ "", "", ":::split", "", "" }), i(0), t({ "", "", ":::", "" }) }
+	),
+	s({ trig = "split" }, { t({ ":::split", "", "" }), i(0), t({ "", "", ":::", "" }) }),
+	s({ trig = "even" }, { t({ ":::columns", "", "" }) }),
+	s({ trig = "date" }, { t({ ":::date", "" }) }),
+	-- s({ trig = "h4" }, { t({ "#### " }), i(0) }),
+}
