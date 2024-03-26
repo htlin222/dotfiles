fef69d6ffb2cbf2a4278b1821f99163903a4242f
htlin222 <hsieh.ting.lin@gmail.com>
2024-03-26 17:27:30 +0800

    style: Update code formatting in markdown snippets for Marp

--- neovim/snippets/markdown/marp.lua
+++ neovim/snippets/markdown/marp.lua
@@ -16,5 +16,6 @@ return {
 	s({ trig = "split" }, { t({ "", ":::split", "", "" }), i(0), t({ "", "", ":::", "" }) }),
 	s({ trig = "even" }, { t({ "", ":::columns", "", "" }) }),
 	s({ trig = "date" }, { t({ "", ":::date", "" }) }),
-	-- s({ trig = "h4" }, { t({ "#### " }), i(0) }),
+	s({ trig = "shadow" }, { t({ "drop-shadow:0px,45px,30px,rgba(0,0,0,.7) Figure: width:1150px" }), i(0) }),
+	s({ trig = "opacity" }, { t({ "bg opacity:.3 h:500px" }), i(0) }),
 }
