95839325f2311670f9fa32d5e74af75a7faa8b3d
Hsieh-Ting Lin <htlin@Hsieh-TingdeMac-mini.local>
2024-08-12 00:16:54 +0800

    Update marp engine path to src/engine.js

--- shellscripts/to_html.sh
+++ shellscripts/to_html.sh
@@ -5,24 +5,24 @@ input_file="${1%.*}"
 slug=$(awk -F 'slug: ' '/slug: /{print $2}' "$1" | sed 's/"//g')
 # 檢查 slug 是否為空
 if [ -z "$slug" ]; then
-	# 如果 slug 為空，則設定 filename 變數為輸入檔案名（不含副檔名）
-	filename=$input_file
+  # 如果 slug 為空，則設定 filename 變數為輸入檔案名（不含副檔名）
+  filename=$input_file
 else
-	# 否則，使用 slug
-	filename=$slug
+  # 否則，使用 slug
+  filename=$slug
 fi
 if [ -d "./output/$filename" ]; then
-	read -p "The folder already exists. Do you want to overwrite it? (y/n) " answer
-	if [ "$answer" != "y" ]; then
-		echo "Exiting..."
-		exit 1
-	fi
+  read -p "The folder already exists. Do you want to overwrite it? (y/n) " answer
+  if [ "$answer" != "y" ]; then
+    echo "Exiting..."
+    exit 1
+  fi
 fi
 mkdir -p "./output/$filename"
-cp -r -P "./src" "./output/$filename/"
+# cp -r -P "./src" "./output/$filename/"
 # cp "$1" "./output/$filename/backup_$(date '+%Y-%m-%d-%H-%M').md"
 # mv "$1" "./$filename/"
 
-marp --theme ./themes/my-theme.css "$1" --engine engine.js --bespoke.progress --html -o "./output/$filename/index.html"
+marp --theme ./themes/my-theme.css "$1" --engine src/engine.js --bespoke.progress --html -o "./output/$filename/index.html"
 
 exit 0
