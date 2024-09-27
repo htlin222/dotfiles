12b1ad80aa17beaa9a0bc360846e62ad94588990
htlin222 <hsieh.ting.lin@gmail.com>
2024-09-28 01:00:52 +0800

    fix: Update website link in shell script to point to new domain "oncologist.tw"

--- shellscripts/publi.sh
+++ shellscripts/publi.sh
@@ -42,7 +42,7 @@ if git -C ~/blog/ rev-parse --git-dir >/dev/null 2>&1; then
   git -C ~/blog/ commit -m "routine blogging ✏️ "
   git -C ~/blog/ push
   echo "👉 see action at https://app.netlify.com/sites/htlin/deploys"
-  echo "👉 see website at https://htlin.site"
+  echo "👉 see website at https://oncologist.tw"
 else
   echo "🔔 Not a git repo"
 fi
