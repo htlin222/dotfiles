38e0537f4a1b008e5904a57afe34d1ffa7060e76
Hsieh-Ting Lin <htlin@Hsieh-TingdeMac-mini.local>
2024-11-19 02:50:48 +0800

    Add helpful aliases for project management tasks

--- zsh/modules/alias.zsh
+++ zsh/modules/alias.zsh
@@ -12,6 +12,9 @@ alias ztop='zenith'
 alias f="fix"
 alias ec="echo"
 alias nt='newsboat'
+alias claupub="cd ~/claude_lab && npm run build && netlify deploy --prod --dir=dist"
+alias ripnetlify="rip ./.netlify"
+alias artifact="cd ~/claude_lab/src/ && vim ~/claude_lab/src/App.jsx"
 alias todo='pter ~/Dropbox/todo.txt'
 alias zshconfig="vim ~/.zshrc"
 alias clens="csvlens"
