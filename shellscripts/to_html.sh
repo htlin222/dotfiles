959152d5b05be2b1597a7f5dc97a94813d33612a
Hsieh-Ting Lin (林協霆) <61491860+htlin222@users.noreply.github.com>
2025-01-15 11:33:11 +0800

    feat: Update to_html script to generate HTML file in the same directory as input file

--- shellscripts/to_html.sh
+++ shellscripts/to_html.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 # title: to_html
 # date created: "2023-09-30"
-input_file="${1%.*}"
-slug=$(awk -F 'slug: ' '/slug: /{print $2}' "$1" | sed 's/"//g')
-# 檢查 slug 是否為空
-if [ -z "$slug" ]; then
-  # 如果 slug 為空，則設定 filename 變數為輸入檔案名（不含副檔名）
-  filename=$input_file
-else
-  # 否則，使用 slug
-  filename=$slug
-fi
-if [ -d "./output/$filename" ]; then
-  read -p "The folder already exists. Do you want to overwrite it? (y/n) " answer
-  if [ "$answer" != "y" ]; then
-    echo "Exiting..."
-    exit 1
-  fi
-fi
-mkdir -p "./output/$filename"
-# cp -r -P "./src" "./output/$filename/"
-# cp "$1" "./output/$filename/backup_$(date '+%Y-%m-%d-%H-%M').md"
-# mv "$1" "./$filename/"
 
-marp --theme "$HOME/Dropbox/slides/contents/themes/main.css" "$1" --engine "$HOME/Dropbox/slides/src/engine.js" --bespoke.progress --html -o "$filename/index.html"
+marp --theme "$HOME/Dropbox/slides/contents/themes/main.css" "$1" --engine "$HOME/Dropbox/slides/src/engine.js" --bespoke.progress --html -o "${1%.*}/index.html"
+
+cd ${1%.*}
 
 exit 0
