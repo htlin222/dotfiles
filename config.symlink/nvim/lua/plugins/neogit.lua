abdf16de01632295864b2dba3f3857ec93d6247b
Hsieh-Ting Lin (林協霆) <61491860+htlin222@users.noreply.github.com>
2025-01-28 10:41:34 +0800

    feat: Add new plugins and configurations for GitGraph and Neogit plugins

---
+++ config.symlink/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return {
+  "NeogitOrg/neogit",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = true,
+}
