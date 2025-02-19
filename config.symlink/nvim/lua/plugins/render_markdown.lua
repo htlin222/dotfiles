cb578e1d955f32ea21b0186a8e6e4120825cae05
Hsieh-Ting Lin (林協霆) <61491860+htlin222@users.noreply.github.com>
2025-02-20 01:47:50 +0800

    Update theme to "jellybeans" and enable list bullet rendering

--- config.symlink/nvim/lua/plugins/render_markdown.lua
+++ config.symlink/nvim/lua/plugins/render_markdown.lua
@@ -3,11 +3,36 @@ return {
   "MeanderingProgrammer/markdown.nvim",
   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
   -- ft = { "markdown", "quarto", "qmd" },
-  -- event = "VeryLazy",
+  event = "VeryLazy",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
     require("render-markdown").setup {
       file_types = { "markdown", "quarto" },
+      bullet = {
+        -- Turn on / off list bullet rendering
+        enabled = true,
+        -- Additional modes to render list bullets
+        render_modes = false,
+        -- Replaces '-'|'+'|'*' of 'list_item'
+        -- How deeply nested the list is determines the 'level', how far down at that level determines the 'index'
+        -- If a function is provided both of these values are provided in the context using 1 based indexing
+        -- If a list is provided we index into it using a cycle based on the level
+        -- If the value at that level is also a list we further index into it using a clamp based on the index
+        -- If the item is a 'checkbox' a conceal is used to hide the bullet instead
+        icons = { "●", "○", "◆", "◇" },
+        -- Replaces 'n.'|'n)' of 'list_item'
+        -- How deeply nested the list is determines the 'level', how far down at that level determines the 'index'
+        -- If a function is provided both of these values are provided in the context using 1 based indexing
+        -- If a list is provided we index into it using a cycle based on the level
+        -- If the value at that level is also a list we further index into it using a clamp based on the index
+        ordered_icons = function(ctx)
+          local value = vim.trim(ctx.value)
+          local index = tonumber(value:sub(1, #value - 1))
+          return string.format("%d.", index > 1 and index or ctx.index)
+        end,
+        right_pad = 1,
+        highlight = "RenderMarkdownBullet",
+      },
       win_options = {
         -- See :h 'conceallevel'
         conceallevel = {
